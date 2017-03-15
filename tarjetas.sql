bef434178a7d851440c7cdc98acd1c92d6d443c5
Andres Galvez <pocyxDesigner@hotmail.com>
2017-03-15 13:18:45 +0100

    completo

--- tarjetas.sql
+++ tarjetas.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 23-08-2014 a las 11:18:07
--- Versión del servidor: 5.5.38-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
