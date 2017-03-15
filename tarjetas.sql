7cb0cb2aa09fa7b53aa4e5bba4eb3b1b1917fb2c
Andres Galvez <pocyxDesigner@hotmail.com>
2017-03-15 14:55:24 +0100

    completo

--- tarjetas.sql
+++ tarjetas.sql
@@ -35,10 +35,34 @@ CREATE TABLE IF NOT EXISTS `tarjeta_sonido` (
 --
 
 INSERT INTO `tarjeta_sonido` (`tarjeta_id`, `tipo`, `marca`, `modelo`, `precio`) VALUES
-(1, 'Controladora', 'Numark', 'sdrk45', 2005.00),
-(2, 'Externa', 'ark', 'siugi565', 25.00),
-(3, 'Interna', 'Numa', 'lgk87', 48.00),
-(4, 'Controladora', 'Numark','oih7', 2005.00);
+
+(1, 'Interna', 'Speedlink', '4CH', 7.50),
+(2, 'Interna', 'Power Star', 'CS-OEM-51', 9.90),
+(3, 'Interna', 'ISA', '3D', 10.95),
+(4, 'Interna', 'Asus', 'DGX', 34.66),
+(5, 'Interna', 'Creative Sound', 'BLASTER Z', 81.22),
+(6, 'Interna', 'CLS', '6CHANEL 3D', 11.85),
+(7, 'Interna', 'RADEON', 'R9 NANO 4GB HBM PCIE', 875.00),
+(8, 'Interna', 'ASUS', 'XONAR DX PCIE', 398.12),
+(9, 'Interna', 'Creative Sound', 'BLASTER ZXR PCIE', 301.84),
+(10, 'Externa', 'SODIAL', '7120', 1.40),
+(11, 'Externa', 'TOOGOO', '3D', 1.62),
+(12, 'Externa', '5H', 'V2', 6.11),
+(13, 'Externa', 'Playvision', 'j41s', 54.99),
+(14, 'Externa', 'Ch Sound', 'AIRCRAFT', 11.67),
+(15, 'Externa', 'Playvision', 'AOEYOO', 40.98),
+(16, 'Externa', 'DAC', '80', 737.25),
+(17, 'Externa', 'Creative Sound', 'LAB RECON3D', 675.00),
+(18, 'Externa', 'DAC', 'AUDIOTRAK', 520.11),
+(19, 'Controladora', 'Hello Kitty', '', 30.00),
+(20, 'Controladora', 'Hercules', 'CONTROL COMPACT', 43.50),
+(21, 'Controladora', 'Hercules', 'CONTROL GLOW', 67.01),
+(22, 'Controladora', 'Hercules', '4780833', 79.47),
+(23, 'Controladora', 'Hercules', 'AIR+', 149.00),
+(24, 'Controladora', 'Hercules', '4780773', 183.95),
+(25, 'Controladora', 'Pioneer', 'RZX', 3060.20),
+(26, 'Controladora', 'Numark', 'NS7', 2005.00),
+(27, 'Controladora', 'AVID','ARTIST CONTROL', 1279.45);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
